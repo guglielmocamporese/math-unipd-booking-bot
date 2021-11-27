91db17c3967b90b5151d871d405a0a0f60aa826c
guglielmocamporese <guglielmocamporese@gmail.com>
2021-11-27 13:05:26 +0100

    improved bot

--- run_bot.py
+++ run_bot.py
@@ -10,38 +10,27 @@ def run_bash_cmd(cmd):
     output, error = process.communicate()
     return output.decode('utf8'), error
 
-def book(update, context):
+def _act(update, context, bash_file):
     args = ' ' + ' '.join(context.args)
     args = args.replace(' —', ' --')
     if ('user' not in args) and ('pwd' not in args):
         context.bot.send_message(update.message.chat_id, 'You must provide the math "user" and "pwd".')
     else:
-        out, err = run_bash_cmd('./scripts/book' + args)
+        out, err = run_bash_cmd(bash_file + args)
         context.bot.send_message(update.message.chat_id, out)
+
+def book(update, context):
+    _act(update, context, './scripts/book')
 
 def check(update, context):
-    args = ' ' + ' '.join(context.args)
-    args = args.replace(' —', ' --')
-    if ('user' not in args) and ('pwd' not in args):
-        context.bot.send_message(update.message.chat_id, 'You must provide the math "user" and "pwd".')
-    else:
-        out, err = run_bash_cmd('./scripts/check' + args)
-        context.bot.send_message(update.message.chat_id, out)
+    _act(update, context, './scripts/check')
 
 def remove(update, context):
-    args = ' ' + ' '.join(context.args)
-    args = args.replace(' —', ' --')
-    if ('user' not in args) and ('pwd' not in args):
-        context.bot.send_message(update.message.chat_id, 'You must provide the math "user" and "pwd".')
-    else:
-        out, err = run_bash_cmd('./scripts/remove' + args)
-        context.bot.send_message(update.message.chat_id, out)
+    _act(update, context, './scripts/remove')
 
 def start(update, context):
     chat_id = update.message.chat_id
     first_name = update.message.chat.first_name
-    last_name = update.message.chat.last_name
-    username = update.message.chat.username
     context.bot.send_message(chat_id, f'Welcome {first_name}')
 
 def main():
