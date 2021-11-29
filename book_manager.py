742d029877a20ba054347f2df08438f1916d2056
guglielmocamporese <guglielmocamporese@gmail.com>
2021-11-29 12:06:47 +0100

    fixed identation

--- book_manager.py
+++ book_manager.py
@@ -9,197 +9,196 @@ from selenium.webdriver.common.keys import Keys
 from utils import load_page
 
 HTML_FORM_IDS = {
-	'year': 'tyear',
-	'month': 'tmonth',
-	'day': 'tday',
-	'arrival': 'tbegin',
-	'departure': 'tend',
-	'office': 'tufficio',
-	'guests': 'tguest',
+    'year': 'tyear',
+    'month': 'tmonth',
+    'day': 'tday',
+    'arrival': 'tbegin',
+    'departure': 'tend',
+    'office': 'tufficio',
+    'guests': 'tguest',
 }
 VERBOSE = True
 
 def book_single_day(user, pwd, args):
-	"""
-	Book the office on a single day.
-	"""
-	driver = load_page(user, pwd)
-	for k in ['year', 'month', 'day']:
-		search = driver.find_elements(By.NAME, HTML_FORM_IDS[k])[0]
-		search.send_keys(args[k])
-	search.send_keys(Keys.RETURN)
+    """
+    Book the office on a single day.
+    """
+    driver = load_page(user, pwd)
+    for k in ['year', 'month', 'day']:
+        search = driver.find_elements(By.NAME, HTML_FORM_IDS[k])[0]
+        search.send_keys(args[k])
+    search.send_keys(Keys.RETURN)
 
-	search = driver.find_elements(By.TAG_NAME, 'table')[0]
-	search = search.find_elements(By.TAG_NAME, 'tr')
-	reservation_already_done = len(search) > 1
+    search = driver.find_elements(By.TAG_NAME, 'table')[0]
+    search = search.find_elements(By.TAG_NAME, 'tr')
+    reservation_already_done = len(search) > 1
 
-	if reservation_already_done:
-		h0, h1, _, office = search[1].text.split()
-		day_str = datetime.datetime(args['year'], datetime.datetime.strptime(args['month'], '%B').month, 
-											args['day']).strftime('%a')
-		print(f'You have already booked the office {office} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} '
-			  f'from {int(h0)}:00 to {int(h1)}:00.')
-	
-	else:
-		for k in ['arrival', 'departure', 'office', 'guests']:
-			search = driver.find_elements(By.NAME, HTML_FORM_IDS[k])[0]
-			search.send_keys(args[k])
-		search.send_keys(Keys.RETURN)
+    if reservation_already_done:
+        h0, h1, _, office = search[1].text.split()
+        day_str = datetime.datetime(args['year'], datetime.datetime.strptime(args['month'], '%B').month, 
+                                                                                args['day']).strftime('%a')
+        print(f'You have already booked the office {office} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} '
+                  f'from {int(h0)}:00 to {int(h1)}:00.')
+    
+    else:
+        for k in ['arrival', 'departure', 'office', 'guests']:
+            search = driver.find_elements(By.NAME, HTML_FORM_IDS[k])[0]
+            search.send_keys(args[k])
+        search.send_keys(Keys.RETURN)
 
-		search = driver.find_elements(By.CLASS_NAME, 'sansserif')
-		reservation_done = not any(['Access not permitted' in s.text for s in search]) 
-		if VERBOSE:
-			if reservation_done:
-				day_str = datetime.datetime(args['year'], datetime.datetime.strptime(args['month'], '%B').month, 
-											args['day']).strftime('%a')
-				print(f'Reservation on {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} ' 
-					  f'from {args["arrival"]}:00 to {args["departure"]}:00 in the office {args["office"]} done!')
-			else:
-				print('Reservation not done.')
-	driver.quit()
+        search = driver.find_elements(By.CLASS_NAME, 'sansserif')
+        reservation_done = not any(['Access not permitted' in s.text for s in search]) 
+        if VERBOSE:
+            if reservation_done:
+                day_str = datetime.datetime(args['year'], datetime.datetime.strptime(args['month'], '%B').month, 
+                                                                        args['day']).strftime('%a')
+                print(f'Reservation on {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} ' 
+                          f'from {args["arrival"]}:00 to {args["departure"]}:00 in the office {args["office"]} done!')
+            else:
+                print('Reservation not done.')
+    driver.quit()
 
 def check_single_day(user, pwd, args):
-	"""
-	Book the office on a single day.
-	"""
-	driver = load_page(user, pwd)
-	for k in ['year', 'month', 'day']:
-		search = driver.find_elements(By.NAME, HTML_FORM_IDS[k])[0]
-		search.send_keys(args[k])
-	search.send_keys(Keys.RETURN)
+    """
+    Book the office on a single day.
+    """
+    driver = load_page(user, pwd)
+    for k in ['year', 'month', 'day']:
+        search = driver.find_elements(By.NAME, HTML_FORM_IDS[k])[0]
+        search.send_keys(args[k])
+    search.send_keys(Keys.RETURN)
 
-	search = driver.find_elements(By.TAG_NAME, 'table')[0]
-	search = search.find_elements(By.TAG_NAME, 'tr')
-	reservation_already_done = len(search) > 1
-	day_str = datetime.datetime(args['year'], datetime.datetime.strptime(args['month'], '%B').month, 
-								args['day']).strftime('%a')
-	if reservation_already_done:
-		h0, h1, _, office = search[1].text.split()
-		print(f'You have already booked the office {office} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} '
-			  f'from {int(h0)}:00 to {int(h1)}:00.')
-	else:
-
-		print(f'You have NOT booked the office {args["office"]} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]}.')
-	driver.quit()
+    search = driver.find_elements(By.TAG_NAME, 'table')[0]
+    search = search.find_elements(By.TAG_NAME, 'tr')
+    reservation_already_done = len(search) > 1
+    day_str = datetime.datetime(args['year'], datetime.datetime.strptime(args['month'], '%B').month, 
+                                                            args['day']).strftime('%a')
+    if reservation_already_done:
+        h0, h1, _, office = search[1].text.split()
+        print(f'You have already booked the office {office} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} '
+                  f'from {int(h0)}:00 to {int(h1)}:00.')
+    else:
+        print(f'You have NOT booked the office {args["office"]} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]}.')
+    driver.quit()
 
 def remove_single_day(user, pwd, args):
-	"""
-	Remove booking on a single day.
-	"""
-	driver = load_page(user, pwd)
-	for k in ['year', 'month', 'day']:
-		search = driver.find_elements(By.NAME, HTML_FORM_IDS[k])[0]
-		search.send_keys(args[k])
-	search.send_keys(Keys.RETURN)
+    """
+    Remove booking on a single day.
+    """
+    driver = load_page(user, pwd)
+    for k in ['year', 'month', 'day']:
+        search = driver.find_elements(By.NAME, HTML_FORM_IDS[k])[0]
+        search.send_keys(args[k])
+    search.send_keys(Keys.RETURN)
 
-	search = driver.find_elements(By.TAG_NAME, 'table')[0]
-	search = search.find_elements(By.TAG_NAME, 'tr')
-	reservation_already_done = len(search) > 1
-	day_str = datetime.datetime(args['year'], datetime.datetime.strptime(args['month'], '%B').month, 
-								args['day']).strftime('%a')
-	if reservation_already_done:
-		search = driver.find_elements(By.ID, 'chooseday')[0]
-		search.click()
-		search = driver.find_elements(By.CLASS_NAME, 'sansserif')
-		removed = any(['Delete successful' in s.text for s in search])
-		if removed:
-			print(f'Booking in office {args["office"]} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} removed.')
-		else:
-			print(f'Booking in office {args["office"]} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} NOT removed.')
-	else:
-		print(f'You have NOT booked the office {args["office"]} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]}.')
-	driver.quit()
+    search = driver.find_elements(By.TAG_NAME, 'table')[0]
+    search = search.find_elements(By.TAG_NAME, 'tr')
+    reservation_already_done = len(search) > 1
+    day_str = datetime.datetime(args['year'], datetime.datetime.strptime(args['month'], '%B').month, 
+                                                            args['day']).strftime('%a')
+    if reservation_already_done:
+        search = driver.find_elements(By.ID, 'chooseday')[0]
+        search.click()
+        search = driver.find_elements(By.CLASS_NAME, 'sansserif')
+        removed = any(['Delete successful' in s.text for s in search])
+        if removed:
+            print(f'Booking in office {args["office"]} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} removed.')
+        else:
+            print(f'Booking in office {args["office"]} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]} NOT removed.')
+    else:
+        print(f'You have NOT booked the office {args["office"]} for {day_str} {args["day"]}-{args["month"][:3]}-{args["year"]}.')
+    driver.quit()
 
 class BookManager:
-	def __init__(self, user, pwd, args, mode=None):
-		self.user = user
-		self.pwd = pwd
-		self.args = args
-		assert mode in ['book', 'check', 'remove']
-		self.mode = mode
+    def __init__(self, user, pwd, args, mode=None):
+        self.user = user
+        self.pwd = pwd
+        self.args = args
+        assert mode in ['book', 'check', 'remove']
+        self.mode = mode
 
-	def run(self):
-		"""
-		Run the book manager (book reservations, check reservations, remove ressrvations).
-		"""
-		days = [datetime.date.today()]
-		if self.args['this_week']:
-			day = days[-1] + datetime.timedelta(1)
-			while True:
-				if day.strftime('%a') in ['Sat', 'Sun']:
-					break
-				days += [day]
-				day += datetime.timedelta(1)
-		
-		elif self.args['next_week']:
-			day = days[-1] + datetime.timedelta(1)
-			weekend_passed = False
-			while True:
-				if day.strftime('%a') not in ['Sat', 'Sun']:
-					days += [day]
-				elif day.strftime('%a') == 'Sat':
-					if weekend_passed:
-						break
-				elif day.strftime('%a') == 'Sun':
-					weekend_passed = True
-				day += datetime.timedelta(1)
+    def run(self):
+        """
+        Run the book manager (book reservations, check reservations, remove ressrvations).
+        """
+        days = [datetime.date.today()]
+        if self.args['this_week']:
+            day = days[-1] + datetime.timedelta(1)
+            while True:
+                if day.strftime('%a') in ['Sat', 'Sun']:
+                    break
+                days += [day]
+                day += datetime.timedelta(1)
+        
+        elif self.args['next_week']:
+            day = days[-1] + datetime.timedelta(1)
+            weekend_passed = False
+            while True:
+                if day.strftime('%a') not in ['Sat', 'Sun']:
+                    days += [day]
+                elif day.strftime('%a') == 'Sat':
+                    if weekend_passed:
+                            break
+                elif day.strftime('%a') == 'Sun':
+                    weekend_passed = True
+                day += datetime.timedelta(1)
 
-		elif self.args['this_month']:
-			day = days[-1] + datetime.timedelta(1)
-			year, month = datetime.date.today().year, datetime.date.today().month
-			while True:
-				if (day.year * 12 + day.month - (year * 12 + month)) == 1:
-					break
-				if day.strftime('%a') not in ['Sat', 'Sun']:
-					days += [day]
-				day += datetime.timedelta(1)
+        elif self.args['this_month']:
+            day = days[-1] + datetime.timedelta(1)
+            year, month = datetime.date.today().year, datetime.date.today().month
+            while True:
+                if (day.year * 12 + day.month - (year * 12 + month)) == 1:
+                    break
+                if day.strftime('%a') not in ['Sat', 'Sun']:
+                    days += [day]
+                day += datetime.timedelta(1)
 
-		elif self.args['next_month']:
-			day = days[-1] + datetime.timedelta(1)
-			year, month = datetime.date.today().year, datetime.date.today().month
-			while True:
-				if (day.year * 12 + day.month - (year * 12 + month)) == 2:
-					break
-				if day.strftime('%a') not in ['Sat', 'Sun']:
-					days += [day]
-				day += datetime.timedelta(1)
+        elif self.args['next_month']:
+            day = days[-1] + datetime.timedelta(1)
+            year, month = datetime.date.today().year, datetime.date.today().month
+            while True:
+                if (day.year * 12 + day.month - (year * 12 + month)) == 2:
+                    break
+                if day.strftime('%a') not in ['Sat', 'Sun']:
+                    days += [day]
+                day += datetime.timedelta(1)
 
-		else:
-			if self.mode == 'book':
-				return book_single_day(self.user, self.pwd, self.args)
-			elif self.mode == 'check':
-				return check_single_day(self.user, self.pwd, self.args)
-			elif self.mode == 'remove':
-				return remove_single_day(self.user, self.pwd, self.args)
+        else:
+            if self.mode == 'book':
+                return book_single_day(self.user, self.pwd, self.args)
+            elif self.mode == 'check':
+                return check_single_day(self.user, self.pwd, self.args)
+            elif self.mode == 'remove':
+                return remove_single_day(self.user, self.pwd, self.args)
 
-		out = []
-		for day in days:
-			args_day = self.args
-			args_day['year'], args_day['month'], args_day['day'] = day.year, day.month, day.day
-			args_day['month'] = datetime.date(1900, args['month'] , 1).strftime('%B')
-			if self.mode == 'book':
-				out_i = book_single_day(self.user, self.pwd, args_day)
-			elif self.mode == 'check':
-				out_i = check_single_day(self.user, self.pwd, args_day)
-			elif self.mode == 'remove':
-				out_i = remove_single_day(self.user, self.pwd, args_day)
-			out += [out_i]
-		return out
+        out = []
+        for day in days:
+            args_day = self.args
+            args_day['year'], args_day['month'], args_day['day'] = day.year, day.month, day.day
+            args_day['month'] = datetime.date(1900, args['month'] , 1).strftime('%B')
+            if self.mode == 'book':
+                out_i = book_single_day(self.user, self.pwd, args_day)
+            elif self.mode == 'check':
+                out_i = check_single_day(self.user, self.pwd, args_day)
+            elif self.mode == 'remove':
+                out_i = remove_single_day(self.user, self.pwd, args_day)
+            out += [out_i]
+        return out
 
 
 if __name__ == '__main__':
-	import sys
-	import getpass
-	from utils import get_args
+    import sys
+    import getpass
+    from utils import get_args
 
-	# Retrieve input args
-	args = get_args(sys.argv[1:])
+    # Retrieve input args
+    args = get_args(sys.argv[1:])
 
-	# Retrieve math credentials
-	user = input('[Username] ') if len(args['user']) == 0 else args['user']
-	pwd = getpass.getpass('[Password] ') if len(args['pwd']) == 0 else args['pwd']
-        
+    # Retrieve math credentials
+    user = input('[Username] ') if len(args['user']) == 0 else args['user']
+    pwd = getpass.getpass('[Password] ') if len(args['pwd']) == 0 else args['pwd']
+    
 
-	# Book
-	bm = BookManager(user, pwd, args, mode=args['mode'])
-	bm.run()
+    # Book
+    bm = BookManager(user, pwd, args, mode=args['mode'])
+    bm.run()
--- utils.py
+++ utils.py
@@ -10,55 +10,53 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-#CHROME_DRIVER_PATH = os.path.abspath('./chromedriver') # path of chromedriver
 import chromedriver_binary
 
 def load_page(user, pwd):
-	"""
-	Load the booking web-page given the math.unipd credentials.
-	"""
-	# Load page
+    """
+    Load the booking web-page given the math.unipd credentials.
+    """
+    # Load page
 
-	op = webdriver.ChromeOptions()
-	op.add_argument('headless')
-	#driver = webdriver.Chrome(service=Service(CHROME_DRIVER_PATH), options=op)
-	driver = webdriver.Chrome(options=op)
-	driver.get(f'https://{user}:{pwd}@novnc.math.unipd.it/presenze/index.php')
-	return driver
+    op = webdriver.ChromeOptions()
+    op.add_argument('headless')
+    driver = webdriver.Chrome(options=op)
+    driver.get(f'https://{user}:{pwd}@novnc.math.unipd.it/presenze/index.php')
+    return driver
 
 def get_args(stdin):
-	"""
-	Parse input arguments.
-	"""
-	now = datetime.datetime.now()
-	parser = argparse.ArgumentParser(stdin)
-	parser.add_argument('--mode', type=str, default='book', help='Year of the booking.', choices=['book', 'check', 'remove'])
-	parser.add_argument('--year', '-y', type=int, default=now.year, 
-						help='Year of the booking.', choices=[2020, 2021, 2022])
-	parser.add_argument('--month', '-m', type=int, default=now.month, 
-						help='Month number of the booking.', choices=list(range(1, 13)))
-	parser.add_argument('--day', '-d', type=int, default=now.day, help='Day of the booking.')
-	parser.add_argument('--arrival', '-ta', type=int, default=9, 
-						help='Time of arrival (h) of the booking.', choices=list(range(7, 19)))
-	parser.add_argument('--departure', '-td', type=int, default=18, 
-						help='Time of departure (h) of the booking.', choices=list(range(8, 20)))
-	parser.add_argument('--office', '-o', type=int, default=732, help='Office number.')
-	parser.add_argument('--guests', '-g', type=str, default='', help='Guests names (optional).')
+    """
+    Parse input arguments.
+    """
+    now = datetime.datetime.now()
+    parser = argparse.ArgumentParser(stdin)
+    parser.add_argument('--mode', type=str, default='book', help='Year of the booking.', choices=['book', 'check', 'remove'])
+    parser.add_argument('--year', '-y', type=int, default=now.year, 
+                                            help='Year of the booking.', choices=[2020, 2021, 2022])
+    parser.add_argument('--month', '-m', type=int, default=now.month, 
+                                            help='Month number of the booking.', choices=list(range(1, 13)))
+    parser.add_argument('--day', '-d', type=int, default=now.day, help='Day of the booking.')
+    parser.add_argument('--arrival', '-ta', type=int, default=9, 
+                                            help='Time of arrival (h) of the booking.', choices=list(range(7, 19)))
+    parser.add_argument('--departure', '-td', type=int, default=18, 
+                                            help='Time of departure (h) of the booking.', choices=list(range(8, 20)))
+    parser.add_argument('--office', '-o', type=int, default=732, help='Office number.')
+    parser.add_argument('--guests', '-g', type=str, default='', help='Guests names (optional).')
 
-	parser.add_argument('--today', action='store_true', help='Use today as the time reference.')
-	parser.add_argument('--tomorrow', action='store_true', help='Use tomorrow as the time reference.')
-	parser.add_argument('--this_week', action='store_true', help='Use this week as the time reference.')
-	parser.add_argument('--next_week', action='store_true', help='Use next week as the time reference.')
-	parser.add_argument('--this_month', action='store_true', help='Use this month as the time reference.')
-	parser.add_argument('--next_month', action='store_true', help='Use next month as the time reference.')
-	parser.add_argument('--user', type=str, default='', help='Math username.')
-	parser.add_argument('--pwd', type=str, default='', help='Math password.')
-	args = vars(parser.parse_args())
+    parser.add_argument('--today', action='store_true', help='Use today as the time reference.')
+    parser.add_argument('--tomorrow', action='store_true', help='Use tomorrow as the time reference.')
+    parser.add_argument('--this_week', action='store_true', help='Use this week as the time reference.')
+    parser.add_argument('--next_week', action='store_true', help='Use next week as the time reference.')
+    parser.add_argument('--this_month', action='store_true', help='Use this month as the time reference.')
+    parser.add_argument('--next_month', action='store_true', help='Use next month as the time reference.')
+    parser.add_argument('--user', type=str, default='', help='Math username.')
+    parser.add_argument('--pwd', type=str, default='', help='Math password.')
+    args = vars(parser.parse_args())
 
-	# Process input args
-	if args['today']:
-		args['year'], args['month'], args['day'] = now.year, now.month, now.day
-	if args['tomorrow']:
-		args['year'], args['month'], args['day'] = now.year, now.month, now.day + 1
-	args['month'] = datetime.date(1900, args['month'] , 1).strftime('%B') # int to string
-	return args
+    # Process input args
+    if args['today']:
+        args['year'], args['month'], args['day'] = now.year, now.month, now.day
+    if args['tomorrow']:
+        args['year'], args['month'], args['day'] = now.year, now.month, now.day + 1
+    args['month'] = datetime.date(1900, args['month'] , 1).strftime('%B') # int to string
+    return args
